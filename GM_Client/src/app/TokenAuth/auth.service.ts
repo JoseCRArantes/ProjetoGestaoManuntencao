e7758a230c07f7271443350f13ef9b7a27dd503a
JoseCRArantes <62546318+JoseCRArantes@users.noreply.github.com>
2020-10-30 21:10:28 +0000

    Testes no server da empresa, até agora a funcionar.

--- GM_Client/src/app/TokenAuth/auth.service.ts
+++ GM_Client/src/app/TokenAuth/auth.service.ts
@@ -32,9 +32,9 @@ export class AuthService {
   private authStatusListener = new Subject<boolean>();
 
   constructor(private http: HttpClient, private router: Router) {}
-  private TokenAPI = "http://localhost:44334/Token";
+  //private TokenAPI = "http://localhost:44334/Token";
 
-  //private TokenAPI = "http://192.168.0.49:8005/Token";
+  private TokenAPI = "http://192.168.0.49:8005/Token";
   
   // Http Headers
   httpOptions = {
--- GMwebApi/GMwebApi/App_Start/WebApiConfig.cs
+++ GMwebApi/GMwebApi/App_Start/WebApiConfig.cs
@@ -28,16 +28,11 @@ namespace GMwebApi
             //var appXmlType = config.Formatters.XmlFormatter.SupportedMediaTypes.FirstOrDefault(t => t.MediaType == "application/xml");
             //config.Formatters.XmlFormatter.SupportedMediaTypes.Remove(appXmlType);
 
-
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional });
-
-
             //config.EnableCors(cors);
-
-
         }
     }
 }
