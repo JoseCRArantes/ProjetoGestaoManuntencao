3a33dfdc6b3053c49bdf1245584a134b2e3c3423
JoseCRArantes <62546318+JoseCRArantes@users.noreply.github.com>
2020-12-13 16:10:32 +0000

    13Dez2020

--- GM_Client/src/app/shared/environments/environments.ts
+++ GM_Client/src/app/shared/environments/environments.ts
@@ -1,11 +1,13 @@
  /*export const environment = {
   production: false,
   apiUrl: "http://192.168.0.49:8005/api",
-}; 
- */
+}; */
+ 
 
  export const environment = {
   production: false,
   apiUrl: "http://localhost:44334/api",
 }; 
+
+
  
